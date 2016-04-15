102d724e63189f2c36077aa73675aca409d72557
Cherian Mathew <cmathew@chakravyu.org>
2016-04-15 11:52:17 +0200

    #47: Add nanonet artifactory repository for deployment

--- project/Global.scala
+++ project/Global.scala
@@ -2,19 +2,21 @@ import sbt._
 
 object Global {
   // Versions
-  lazy val scVersion = "2.11.7"  
+  lazy val scVersion = "2.11.7"
+  lazy val nanonetArtifactoryBaseUrl = "http://artifactory.openshift.nanonet"
   
   // Repositories
   val localMavenRepository = (
-        "Local Maven Repository" at "file://" +
-    		  Path.userHome.absolutePath +
-    			"/.m2/repository"
-    )
-    
+    "Local Maven Repository" at "file://" +
+    Path.userHome.absolutePath +
+    "/.m2/repository")
+
+  val nanonetMavenRepository = (
+    "Nanonet Maven Repository" at nanonetArtifactoryBaseUrl + "/artifactory/libs-snapshot-local/")
+
   val localIvyRepository = (
-        "Local Ivy Repository" at "file://" +
-    		  Path.userHome.absolutePath +
-    			"/.ivy2/cache"
-    )
+    "Local Ivy Repository" at "file://" +
+    Path.userHome.absolutePath +
+    "/.ivy2/cache")
 
 }
