f3442d96ea9e860119e20df9b4a0b50220d64188
Cherian Mathew <cmathew@chakravyu.org>
2017-07-14 13:21:55 +0200

    AL-198 : Adapt to error handling refactor

--- src/main/scala/org/dcs/remote/RemoteService.scala
+++ src/main/scala/org/dcs/remote/RemoteService.scala
@@ -1,6 +1,6 @@
 package org.dcs.remote
 
-import org.dcs.commons.error.{ErrorConstants, RESTException}
+import org.dcs.commons.error.{DCSException, ErrorConstants}
 
 import scala.reflect.ClassTag
 import scala.util.control.NonFatal
@@ -14,17 +14,17 @@ class  RemoteService {
   def loadService[T](implicit tag: ClassTag[T]): T = {
     try {
       val s = service[T]
-      if(s == None) throw new RESTException(ErrorConstants.DCS201) else s.get
+      if(s == None) throw new DCSException(ErrorConstants.DCS201) else s.get
     } catch {
-      case NonFatal(t) => throw new RESTException(ErrorConstants.DCS201)
+      case NonFatal(t) => throw new DCSException(ErrorConstants.DCS201)
     }
   }
   def loadService[T](serviceImplName: String)(implicit tag: ClassTag[T]) =  {
     try {
       val s = service[T](serviceImplName)
-      if(s == None) throw new RESTException(ErrorConstants.DCS201) else s.get
+      if(s == None) throw new DCSException(ErrorConstants.DCS201) else s.get
     } catch {
-      case NonFatal(t) => throw new RESTException(ErrorConstants.DCS201)
+      case NonFatal(t) => throw new DCSException(ErrorConstants.DCS201)
     }
   }
 
