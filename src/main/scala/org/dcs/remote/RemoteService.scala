e1e26baab26f0b23e2fd42b93ad3f80eac92adb8
Cherian Mathew <cmathew@chakravyu.org>
2017-11-06 14:48:04 +0100

    Add methods to initialse remote service cache

--- src/main/scala/org/dcs/remote/RemoteService.scala
+++ src/main/scala/org/dcs/remote/RemoteService.scala
@@ -1,6 +1,8 @@
 package org.dcs.remote
 
-import org.dcs.commons.error.{DCSException, ErrorConstants}
+import org.dcs.api.service.{ProcessorDetails, ProcessorServiceDefinition, RemoteProcessorService, StatefulRemoteProcessorService}
+import org.dcs.commons.error.{DCSException, ErrorConstants, HttpException}
+import org.dcs.remote.cxf.CxfEndpointUtils
 
 import scala.reflect.ClassTag
 import scala.util.control.NonFatal
@@ -29,6 +31,29 @@ class  RemoteService {
   }
 
   def dispose = close
+
+  override def serviceDetails(processorServiceClassName: String, stateful: Boolean): ProcessorDetails = {
+    service(processorServiceClassName, stateful).details()
+  }
+
+  override def service(processorServiceClassName: String, stateful: Boolean): RemoteProcessorService = {
+    if (stateful)
+      loadService[StatefulRemoteProcessorService](processorServiceClassName)
+    else
+      loadService[RemoteProcessorService](processorServiceClassName)
+  }
+
+  override def service(processorServiceClassName: String): RemoteProcessorService = {
+    val psds: List[ProcessorServiceDefinition] =
+      filterServiceByProperty(CxfEndpointUtils.ClassNameKey, processorServiceClassName)
+
+    if(psds.isEmpty)
+      throw new HttpException(ErrorConstants.DCS301.http(400))
+    else {
+      val psd = psds.head
+      service(psd.processorServiceClassName, psd.stateful)
+    }
+  }
 
 }
 
--- src/main/scala/org/dcs/remote/ServiceTracker.scala
+++ src/main/scala/org/dcs/remote/ServiceTracker.scala
@@ -1,6 +1,6 @@
 package org.dcs.remote
 
-import org.dcs.api.service.ProcessorServiceDefinition
+import org.dcs.api.service.{ProcessorDetails, ProcessorServiceDefinition, RemoteProcessorService}
 
 import scala.reflect.ClassTag
 
@@ -11,5 +11,9 @@ trait ServiceTracker {
   def service[T](serviceImplName: String)(implicit tag: ClassTag[T]): Option[T]
   def filterServiceByProperty(property: String, regex: String): List[ProcessorServiceDefinition]
   def services(): List[ProcessorServiceDefinition]
+  def serviceDetails(processorServiceClassName: String, stateful: Boolean): ProcessorDetails
+  def service(processorServiceClassName: String, stateful: Boolean): RemoteProcessorService
+  def service(processorServiceClassName: String): RemoteProcessorService
+
   def close
 }
